c2719856cb6bf95fc8b9f802d770dd0cf367575e
Mitanshu Sukhwani <mitanshu.sukhwani@gmail.com>
2024-06-20 13:13:12 +0530

    fix path

--- process_metadata.py
+++ process_metadata.py
@@ -2,12 +2,17 @@
 import pandas as pd
 
 # Load the data
+print('Loading the data...')
 data_file = 'kaggle/arxiv-metadata-oai-snapshot.json'
 arxiv_metadata = pd.read_json(data_file, lines=True)
 
 # ## Only save columns id, abstract
+# print('Selecting columns...')
 # arxiv_metadata = arxiv_metadata[['id', 'abstract']]
+# print('Saving the data...')
+# arxiv_metadata.to_parquet('kaggle/arxiv_metadata_id_abstract.parquet')
 
 # Save the data to parquet file
-arxiv_metadata.to_parquet('arxiv_metadata.parquet')
+print('Saving the data...')
+arxiv_metadata.to_parquet('kaggle/arxiv_metadata.parquet')
 
